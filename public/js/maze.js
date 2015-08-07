06f1d54c24644d914cd5265032849b9e8c8e1644
wmoai <wmoaism@gmail.com>
2015-08-07 14:13:23 +0900

    Modify hashtag

--- public/js/maze.js
+++ public/js/maze.js
@@ -230,7 +230,7 @@ $(function() {
       var time = Math.floor((getTime() - this.start) / 1000);
       var score = baseWidth * baseHeight - this.steps - time;
       this.log('ゴール！<br>スコア：'+score
-              + '<a href="https://twitter.com/share" class="twitter-share-button" data-text="モアイ迷宮を脱出 [score:'+score+']" data-count="none" data-hashtags="maze">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document, "script", "twitter-wjs");</script>');
+              + '<a href="https://twitter.com/share" class="twitter-share-button" data-text="モアイ迷宮を脱出 [score:'+score+']" data-count="none" data-hashtags="モアイ迷宮">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document, "script", "twitter-wjs");</script>');
       this.end = true;
     }
   }
